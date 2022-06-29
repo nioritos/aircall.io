70f0db062c37d0d2105cbc8507f4988dac837c8e
Junior Guimarães <77200196+nioritos@users.noreply.github.com>
2022-06-28 23:08:04 -0300

    coding the mobile/responsive configurations...

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+const mobileNav = document.querySelector(".navigator-mobile");
+const menuHamburguer = document.querySelector(".menu-mobile");
+
+menuHamburguer.addEventListener('click', () => {
+    
+})
