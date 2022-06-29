10cb828c73829583641e07ba3d5def6aa135d457
Junior Guimarães <77200196+nioritos@users.noreply.github.com>
2022-06-29 11:06:02 -0300

    added the arrow icon to show many informations | coding the animation... #4

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,13 @@ const menuHamburguer = document.querySelector(".menu-mobile");
 menuHamburguer.addEventListener('click', () => {
     
 })
+
+const dropperWord = document.querySelectorAll(".dropper");
+dropperWord.forEach(element => {
+    element.addEventListener('mouseenter', () => {
+        const arrowIcon = document.querySelectorAll(".dropper-arrow");
+        arrowIcon.forEach(element => {
+            element.classList.add("top-top")
+        })
+    })
+});
